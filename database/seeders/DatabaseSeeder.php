8c56dd81f29de22d654b23bb2150a84ba65d5758
sagnik6969 <72630181+sagnik6969@users.noreply.github.com>
2023-12-24 23:54:06 +0530

    47. Factory & Seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,6 +12,43 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        \App\Models\Book::factory(33)
+            ->create()
+            ->each(function ($book) {
+
+                $numberOfReviews = rand(5, 30);
+                \App\Models\Review::factory()
+                    ->count($numberOfReviews)
+                    ->good()
+                    ->for($book) // For keyword is for 1 to many relationship
+                    ->create();
+
+            });
+
+        \App\Models\Book::factory(33)
+            ->create()
+            ->each(function ($book) {
+
+                $numberOfReviews = rand(5, 30);
+                \App\Models\Review::factory()
+                    ->count($numberOfReviews)
+                    ->average()
+                    ->for($book)
+                    ->create();
+
+            });
+        \App\Models\Book::factory(34)
+            ->create()
+            ->each(function ($book) {
+
+                $numberOfReviews = rand(5, 30);
+                \App\Models\Review::factory()
+                    ->count($numberOfReviews)
+                    ->bad()
+                    ->for($book)
+                    ->create();
+
+            });
         // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
