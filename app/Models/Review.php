d870f6fadbcc426ac978fea1154d5a540611ba62
sagnik6969 <72630181+sagnik6969@users.noreply.github.com>
2023-12-25 13:59:02 +0530

    48. Querying & Associating Related Models

--- app/Models/Review.php
+++ app/Models/Review.php
@@ -8,8 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Review extends Model
 {
     use HasFactory;
-    public function book() {
-          return $this->belongsTo(Book::class);
+
+    protected $fillable = ['review', 'rating'];
+    public function book()
+    {
+        return $this->belongsTo(Book::class);
     }
     // the above function is needed to define one to many relationship
     // review belongs to book
