332c096e82a5fbd8db05d68ebb1c4256af738992
sagnik6969 <72630181+sagnik6969@users.noreply.github.com>
2023-12-26 22:12:29 +0530

    56. Popular or Highest Rated - the View

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -8,11 +8,38 @@
         <div class="form-inline">
             <input class="form-control mr-2" type="text" name="title" placeholder="Search by title"
                 value="{{ request('title') }}">
+            <input type="hidden" name="filter" value="{{ request('filter') }}">
             {{-- request('title') => to get old value of title --}}
             <button class="form-control btn btn-outline-primary mr-2">Search</button>
             <a class="btn btn-outline-secondary form-control" href="{{ route('books.index') }}">Reset</a>
         </div>
     </form>
+
+    @php
+        $filters = [
+            '' => 'Latest',
+            'popular_last_month' => 'Popular Last Month',
+            'popular_last_6months' => 'Popular Last 6 Months',
+            'highest_rated_last_month' => 'Highest Rated Last Month',
+            'highest_rated_last_6months' => 'Highest Rated Last 6 Months',
+        ];
+    @endphp
+    <div class="filter-container d-flex text-center p-2 mt-3 rounded">
+        @foreach ($filters as $key => $label)
+            <div
+                class="d-flex justify-content-center align-items-center flex-even rounded p-2 {{ $key == request('filter') ? 'bg-white shadow-sm' : '' }}">
+                <a class="text-muted text-decoration-none"
+                    href="{{ route('books.index', [
+                        ...request()->query(),
+                        // spread operator => works like js.
+                        // request()->query() contains all the
+                        // query params
+                        'filter' => $key,
+                    ]) }}">{{ $label }}</a>
+            </div>
+        @endforeach
+
+    </div>
 
     @forelse ($books as $book)
         <div class="card mt-3">
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,8 +5,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css"
-        integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.2.1/dist/css/bootstrap.min.css"
+        integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
     <title>Book Review</title>
     <style>
         /* * {
@@ -15,6 +15,14 @@
         .container {
             max-width: 700px
         }
+
+        .filter-container {
+            background-color: rgb(241 245 249);
+        }
+
+        .flex-even {
+            flex: 1;
+        }
     </style>
 </head>
 
@@ -22,14 +30,8 @@
     <div class="container mt-5 mb-2">
         @yield('content')
     </div>
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
-        integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous">
-    </script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js"
-        integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous">
-    </script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js"
-        integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.2.1/dist/js/bootstrap.min.js"
+        integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous">
     </script>
 </body>
 
